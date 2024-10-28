3dcd4bd9e59e72a50c23f8ad2b2f0ed380b8502a
André Rolim <andrerolim.ice@gmail.com>
2024-10-28 20:35:18 -0300

    finished API

--- server.js
+++ server.js
@@ -1,4 +1,77 @@
-import express from 'express'
+import express, { request, response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express()
+const prisma = new PrismaClient();
 
+const app = express();
+app.use(express.json()); /* Garante que o express utilize o .json */
+
+app.post("/usuarios", async (request, response) => {
+  await prisma.user.create({
+    data: {
+      email: request.body.email,
+      name: request.body.name,
+      age: request.body.age,
+    },
+  });
+
+  response.status(201).json(request.body);
+});
+
+app.get("/usuarios", async (request, response) => {
+  let users = [];
+  users = await prisma.user.findMany({
+    where: {
+      name: request.query.name,
+      age: request.query.age,
+      email: request.query.email,
+    },
+  });
+  if (request.query) {
+  } else {
+    users = await prisma.user.findMany();
+  }
+
+  response.status(200).json(users);
+});
+
+app.put("/usuarios/:id", async (request, response) => {
+  await prisma.user.update({
+    where: {
+      id: request.params.id,
+    },
+    data: {
+      email: request.body.email,
+      name: request.body.name,
+      age: request.body.age,
+    },
+  });
+
+  response.status(201).json(request.body);
+});
+
+app.delete("/usuarios/:id", async (request, response) => {
+  await prisma.user.delete({
+    where: {
+      id: request.params.id,
+    },
+  });
+  response.status(200).json({ message: "Usuário deletado com sucesso!" });
+});
+
+app.listen(3000);
+
+/* 
+    O que preciso para criar uma rota?
+    1) Tipo de rota/ Método HTTP
+    2) Endereço
+*/
+
+/* 
+    Criar nossa API de Usuários
+    - Criar usuário
+    - Listar todos os usuários
+    - Editar um usuário
+    - Deletar um usuário
+
+*/
